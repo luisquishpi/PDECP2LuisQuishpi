284ee43d583b00426fd78dbac27f912b369d0cf2
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-14 00:34:44 +0200

    Conexion y ConexionEstado modificado

---
+++ PDECP2LuisQuishpi/src/main/java/state/connection/Conexion.java
@@ -0,0 +1,46 @@
+package state.connection;
+
+public class Conexion {
+    private ConexionEstado conexionEstado;
+
+    public Link link;
+
+    public Conexion(Link link) {
+        assert link != null;
+        this.link = link;
+        conexionEstado.cerrar(this);// Estado.CERRADO;
+    }
+
+    public Link getLink() {
+        return link;
+    }
+
+    public Estado getEstado() {
+        return conexionEstado.getEstado();
+    }
+
+    public void abrir() {
+        conexionEstado.abrir(this);
+    }
+
+    public void cerrar() {
+        conexionEstado.cerrar(this);
+    }
+
+    public void parar() {
+        conexionEstado.parar(this);
+    }
+
+    public void iniciar() {
+        conexionEstado.iniciar(this);
+    }
+
+    public void enviar(String msg) {
+        conexionEstado.enviar(this, msg);
+    }
+
+    public void recibir(int respuesta) {
+        conexionEstado.recibir(this, respuesta);
+    }
+
+}
---
+++ PDECP2LuisQuishpi/src/main/java/state/connection/ConexionEstado.java
@@ -0,0 +1,18 @@
+package state.connection;
+
+public abstract class ConexionEstado {
+
+    public abstract void abrir(Conexion conexion);
+
+    public abstract void cerrar(Conexion conexion);
+
+    public abstract void parar(Conexion conexion);
+
+    public abstract void iniciar(Conexion conexion);
+
+    public abstract void enviar(Conexion conexion, String msg);
+
+    public abstract void recibir(Conexion conexion, int respuesta);
+
+    public abstract Estado getEstado();
+}
