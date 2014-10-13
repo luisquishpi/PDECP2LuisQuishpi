ce413d17215c2079adbd34a53c40d7bcb184d5c1
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-14 00:21:12 +0200

    state.connection: creado ConexionTest y LinkMock

---
+++ PDECP2LuisQuishpi/src/test/java/state/connection/LinkMock.java
@@ -0,0 +1,25 @@
+package state.connection;
+
+
+public final class LinkMock implements Link {
+    private String msg;
+
+    private int respuesta;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public int getRespuesta() {
+        return respuesta;
+    }
+
+    public void enviar(String msg) {
+        this.msg = msg;
+    }
+
+    public void recibir(int respuesta) {
+        this.respuesta = respuesta;
+    }
+
+}
