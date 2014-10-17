1f3a1e1d88c7bfc6a9fd6b1c955c24a26d752a49
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-17 15:04:13 +0200

    Refactor NaturalNumberEs y creado NaturalNumber

---
+++ PDECP2LuisQuishpi/src/main/java/factoryMethod/naturalNumber/NaturalNumber.java
@@ -0,0 +1,34 @@
+package factoryMethod.naturalNumber;
+
+public abstract class NaturalNumber {
+
+    private int value;
+
+    public NaturalNumber(int value) {
+        this.setValue(value);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        assert value >= 0;
+        this.value = value;
+    }
+
+    public void add(int value) {
+        this.setValue(this.value + value);
+    }
+
+    public String getTextValue() {
+        if (this.value < getArrayTextvalue().length) {
+            return getArrayTextvalue()[this.value];
+        } else {
+            return "???";
+        }
+    }
+    public abstract String[] getArrayTextvalue();
+
+    
+}
--- PDECP2LuisQuishpi/src/main/java/factoryMethod/naturalNumber/NaturalNumberEs.java
+++ PDECP2LuisQuishpi/src/main/java/factoryMethod/naturalNumber/NaturalNumberEs.java
@@ -1,32 +1,15 @@
 package factoryMethod.naturalNumber;
 
-public class NaturalNumberEs {
-    private int value;
-
+public class NaturalNumberEs extends NaturalNumber {
+   
     private static final String[] textValue = {"cero", "uno", "dos", "tres", "cuatro", "cinco"};
 
     public NaturalNumberEs(int value) {
-        this.setValue(value);
+        super(value);
     }
 
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        assert value >= 0;
-        this.value = value;
-    }
-
-    public void add(int value) {
-        this.setValue(this.value + value);
-    }
-
-    public String getTextValue() {
-        if (this.value < textValue.length) {
-            return NaturalNumberEs.textValue[this.value];
-        } else {
-            return "???";
-        }
+    @Override
+    public String[] getArrayTextvalue() {
+        return textValue;
     }
 }
